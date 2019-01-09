acec73f8e7e65f9585dc298b3a0a1662a35da1b7
Sophia Anne Marie P. Dizon <sophia.dizon@dss.asia>
2019-01-09 13:36:56 +0800

    initilization included for creating default roles and user if non existing

--- Form1.vb
+++ Form1.vb
@@ -10,7 +10,8 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ConnectDB()
-        ShowData("SELECT * FROM Users")
+        InitializeSetup()
+        'ShowData("SELECT * FROM Users")
 
     End Sub
 
@@ -26,11 +27,66 @@ Public Class Form1
         dbConnection.ConnectionString = "Provider=SQLNCLI11;Server=(LocalDB)\MSSQLLocalDB;Database=GSS;Trusted_Connection=yes;"
     End Sub
 
+
     Private Sub InitializeSetup()
+        CreateDefaultRoles()
+        CreateDefaultUsers()
 
     End Sub
 
+    Private Sub CreateDefaultRoles()
+        Try
+            'Check roles
+            dbAdapter = New OleDbDataAdapter("Select * From Roles", dbConnection)
+            dbDataTable = New DataTable
+            dbAdapter.Fill(dbDataTable)
 
+
+            'If no roles found
+            If (dbDataTable.Rows.Count = 0) Then
+                'insert roles
+                dbAdapter = New OleDbDataAdapter("INSERT INTO Roles (Name) VALUES ('ADMINISTRATOR'), ('MEMBER')", dbConnection)
+                dbAdapter.Fill(dbDataSet)
+
+            End If
+
+        Catch ex As Exception
+            MsgBox("A problem is encountered on the System. Please call your system administrator.")
+            Me.Dispose()
+
+        End Try
+    End Sub
+
+    Private Sub CreateDefaultUsers()
+        Dim adminRoleID As Integer
+
+        'look for Administrator ID
+        Try
+            dbAdapter = New OleDbDataAdapter("Select * From Roles WHERE Name like '%ADMINISTRATOR%'", dbConnection)
+            dbDataTable = New DataTable
+            dbAdapter.Fill(dbDataTable)
+
+            adminRoleID = dbDataTable.Rows(0).Item(0)
+
+            ''Check users
+            dbAdapter = New OleDbDataAdapter("Select * From Users", dbConnection)
+            dbDataTable = New DataTable
+            dbAdapter.Fill(dbDataTable)
+            dbDataSet = New DataSet
+
+            ''If no users found
+            If (dbDataTable.Rows.Count = 0) Then
+                'insert admin
+                dbAdapter = New OleDbDataAdapter("INSERT INTO Users (FirstName, LastName, Username, Password, RoleID) VALUES ('System','Administrator', 'admin','admin'," & adminRoleID & ")", dbConnection)
+                dbAdapter.Fill(dbDataSet)
+                dbDataSet = New DataSet
+
+            End If
+        Catch ex As Exception
+            MsgBox("A problem is encountered on the System. Please call your system administrator.")
+            Me.Dispose()
+        End Try
+    End Sub
 
 
 End Class
